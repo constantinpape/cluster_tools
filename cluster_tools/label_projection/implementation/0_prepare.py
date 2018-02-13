84edf559bf60f3d143d828660e3ef121fdb5a566
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-02-13 11:16:16 -0500

    Add label projection workflow

---
+++ cluster_tools/label_projection/implementation/0_prepare.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+import os
+import argparse
+from math import ceil
+import numpy as np
+
+import z5py
+import nifty
+
+
+def blocks_to_jobs(shape, block_shape, n_jobs, tmp_folder):
+    blocking = nifty.tools.blocking(roiBegin=[0, 0, 0],
+                                    roiEnd=list(shape),
+                                    blockShape=block_shape)
+    n_blocks = blocking.numberOfBlocks
+    assert n_jobs <= n_blocks, "%i, %i" % (n_jobs, n_blocks)
+    chunk_size = int(ceil(float(n_blocks) / n_jobs))
+    block_list = list(range(n_blocks))
+    for idx, i in enumerate(range(0, len(block_list), chunk_size)):
+        np.save(os.path.join(tmp_folder, '1_input_%i.npy' % idx), block_list[i:i + chunk_size])
+    assert idx == n_jobs - 1, "Not enough inputs created: %i / %i" % (idx, n_jobs - 1)
+
+
+def prepare(labels_path, labels_key, out_path, out_key, tmp_folder, block_shape, n_jobs):
+    shape = z5py.File(labels_path)[labels_key].shape
+    out = z5py.File(out_path, use_zarr_format=False)
+    if out_key not in out:
+        out.create_dataset(out_key, dtype='uint64', shape=shape,
+                           chunks=tuple(block_shape),
+                           compression='gzip')
+    else:
+        ds = out[out_key]
+        assert ds.shape == shape
+        assert ds.chunks == tuple(block_shape)
+
+    if not os.path.exists(tmp_folder):
+        os.mkdir(tmp_folder)
+    blocks_to_jobs(shape, block_shape, n_jobs, tmp_folder)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("labels_path", type=str)
+    parser.add_argument("labels_key", type=str)
+    parser.add_argument("out_path", type=str)
+    parser.add_argument("out_key", type=str)
+
+    parser.add_argument("--tmp_folder", type=str)
+    parser.add_argument("--block_shape", nargs=3, type=int)
+    parser.add_argument("--n_jobs", type=int)
+
+    args = parser.parse_args()
+    prepare(args.labels_path, args.labels_key,
+            args.out_path, args.out_key,
+            args.tmp_folder,
+            list(args.block_shape),
+            args.n_jobs)
---
+++ cluster_tools/label_projection/implementation/1_project_labels.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+
+import time
+import os
+import argparse
+import numpy as np
+
+import z5py
+import nifty.distributed as ndist
+
+
+def label_projection_step1(labels_path, labels_key,
+                           out_path, out_key,
+                           node_labeling_path, node_labeling_key,
+                           block_shape, block_file):
+    t0 = time.time()
+    node_labeling = z5py.File(node_labeling_path)[node_labeling_key][:]
+    block_ids = np.load(block_file)
+
+    ndist.nodeLabelingToPixels(os.path.join(labels_path, labels_key),
+                               os.path.join(out_path, out_key),
+                               node_labeling, block_ids,
+                               block_shape)
+
+    job_id = int(os.path.split(block_file)[1].split('_')[2][:-4])
+    print("Success job %i" % job_id)
+    print("In %f s" % (time.time() - t0,))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("labels_path", type=str)
+    parser.add_argument("labels_key", type=str)
+    parser.add_argument("out_path", type=str)
+    parser.add_argument("out_key", type=str)
+    parser.add_argument("node_labeling_path", type=str)
+    parser.add_argument("node_labeling_key", type=str)
+    parser.add_argument("--block_shape", type=int, nargs=3)
+    parser.add_argument("--block_file", type=str)
+    args = parser.parse_args()
+
+    label_projection_step1(args.labels_path, args.labels_key,
+                           args.out_path, args.out_key,
+                           args.node_labeling_path, args.node_labeling_key,
+                           args.block_shape, args.block_file)
