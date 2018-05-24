6b0ac5336137838ddb978d6d08f9e4c294323bbd
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-05-23 22:27:20 -0400

    Fix lauritzen experiments

--- experiments/production/lauritzen_block.py
+++ experiments/production/lauritzen_block.py
@@ -1,7 +1,8 @@
 import os
 import json
 import luigi
-from production import Workflow, write_dt_components_config, write_default_config
+from production import Workflow, Workflow2DWS
+from production import write_dt_components_config, write_default_config
 
 
 def process_lauritzen_block(block_id,
@@ -16,20 +17,21 @@ def process_lauritzen_block(block_id,
 
     cache_prefix = '/groups/saalfeld/home/papec/Work/neurodata_hdd/cache/'
     cache_name_ws = 'lauritzen_production_%i' % block_id
-    cache_name_ws += ws_type
+    cache_name_ws += ('_' + ws_type)
     cache_folder_ws = os.path.join(cache_prefix, cache_name_ws)
 
     cache_name_mc = 'lauritzen_production_%i' % block_id
     mc_string = 'lmc_' if use_lmc else 'mc_'
     mc_string += ws_type
     if use_rf:
-        mc_string += 'rf_'
+        mc_string += '_rf'
     mc_string += '_mcweighted' if weight_mc_edges else '_nomcweighted'
     mc_string += '_mergeweighted' if weight_merge_edges else '_nomergeweighted'
     cache_folder_mc = os.path.join(cache_prefix, cache_name_mc + '_' + mc_string)
 
     if ws_type == 'ws_dt':
         boundary_threshold = .15
+        distance_threshold = 50
     elif 'ws_type' == 'ws_2d':
         boundary_threshold = .2
     else:
@@ -40,13 +42,15 @@ def process_lauritzen_block(block_id,
         if use_rf:
             rf_path = [os.path.join('/groups/saalfeld/home/papec/Work/my_projects/cluster_tools/experiments',
                                     'production/lifted_rf_larger_nh.pkl')]
-    else:
-        lifted_nh = 2
-        if use_rf:
-            rf_path = None
         else:
-            rf_path = [os.path.join('/groups/saalfeld/home/papec/Work/my_projects/cluster_tools/experiments',
+            rf_path = None
+    else:
+        lifted_nh = None
+        if use_rf:
+            rf_path = [os.path.join('/groups/saalfeld/home/papec/Work/my_projects/cluster_tools/experiments/production',
                                     'rf_local_%s.pkl' % etype) for etype in ('xy', 'z')]
+        else:
+            rf_path = None
 
     if weight_mc_edges:
         weighting_exponent = .5
@@ -69,7 +73,8 @@ def process_lauritzen_block(block_id,
                          n_threads=n_threads)
     if ws_type == 'ws_dt':
         write_dt_components_config(config_path,
-                                   boundary_threshold=boundary_threshold)
+                                   boundary_threshold=boundary_threshold,
+                                   distance_threshold=distance_threshold)
 
     seg_key = mc_string
 
@@ -77,6 +82,7 @@ def process_lauritzen_block(block_id,
     #                  'skeletons/neurons_of_interest']
     skeleton_keys = []
     path = '/nrs/saalfeld/lauritzen/0%i/workspace.n5' % block_id
+    wf = Workflow2DWS if ws_type == 'ws_2d' else Workflow
     luigi.run(['--local-scheduler',
                '--path', path,
                '--aff-key', 'raw/predictions/affs_glia',
@@ -88,18 +94,20 @@ def process_lauritzen_block(block_id,
                '--tmp-folder-ws', cache_folder_ws,
                '--tmp-folder-seg', cache_folder_mc,
                '--skeleton-keys', json.dumps(skeleton_keys),
-               '--time-estimate', '10'], Workflow)
+               '--time-estimate', '10'], wf)
 
 
 def grid_search():
     # use_dts = [False, True]
     # use_lmc = [False, True]
-    use_dt = False
-    use_lmc = True
+    ws_type = 'ws_thresh'
+    use_lmc = False
+    use_rf = False
     weight_mc_edges = False
     weight_merge_edges = False
     process_lauritzen_block(2, 300, 12,
-                            use_dt_components=use_dt,
+                            ws_type=ws_type,
+                            use_rf=use_rf,
                             use_lmc=use_lmc,
                             weight_mc_edges=weight_mc_edges,
                             weight_merge_edges=weight_merge_edges)
