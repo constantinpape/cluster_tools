fe18d169007ab05c6d65c7c5a1c4baa9870b2a9f
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-10-02 23:24:13 +0200

    Update downscaling meta-data

--- cluster_tools/downscaling/downscaling_workflow.py
+++ cluster_tools/downscaling/downscaling_workflow.py
@@ -28,27 +28,25 @@ def indent_xml(elem, level=0):
             elem.tail = i
 
 
-# TODO write proper metadata for the format:
-# n5, bdv, or imaris
+# TODO support imaris format
 class WriteDownscalingMetadata(luigi.Task):
     tmp_folder = luigi.Parameter()
     output_path = luigi.Parameter()
     scale_factors = luigi.ListParameter()
     dependency = luigi.TaskParameter()
-    metadata_format = luigi.Parameter(default='custom')
+    metadata_format = luigi.Parameter(default='paintera')
     metadata_dict = luigi.DictParameter(default={})
     output_key_prefix = luigi.Parameter(default='')
 
     def requires(self):
         return self.dependency
 
-    def _write_metadata_custom(self, out_key, scale_factor, effective_scale):
+    def _write_metadata_paintera(self, out_key, scale_factor, effective_scale):
         with file_reader(self.output_path) as f:
             ds = f[out_key]
-            ds.attrs['scale_factor'] = scale_factor
-            ds.attrs['effective_scale'] = effective_scale
+            ds.attrs['downsamplingFactors'] = effective_scale[::-1]
 
-    def _custom_metadata(self):
+    def _paintera_metadata(self):
         effective_scale = [1, 1, 1]
         for scale, scale_factor in enumerate(self.scale_factors):
             # we offset the scale by 1 because
@@ -62,8 +60,14 @@ class WriteDownscalingMetadata(luigi.Task):
                 effective_scale = [eff * scale_factor for eff in effective_scale]
             else:
                 effective_scale = [eff * sf for sf, eff in zip(scale_factor, effective_scale)]
-
-            self._write_metadata_custom(out_key, scale_factor, effective_scale)
+            self._write_metadata_paintera(out_key, effective_scale)
+        # write multiscale attribute and metadata
+        with self.file_reader(self.output_path) as f:
+            f[self.output_key_prefix].attrs["multiScale"] = True
+            resolution = self.metadata_dict.get("resolution", 3 * [1.])[::-1]
+            f[self.output_key_prefix].attrs["resolution"] = resolution
+            offsets = self.metadata_dict.get("offsets", 3 * [1.])[::-1]
+            f[self.output_key_prefix].attrs["offsets"] = offsets
 
     def _write_metadata_bdv(self, scales, chunks):
         # we need to reorder scales and resoultions,
@@ -157,11 +161,11 @@ class WriteDownscalingMetadata(luigi.Task):
                 effective_scale = [eff * scale_factor for eff in effective_scale]
             else:
                 effective_scale = [eff * sf for sf, eff in zip(scale_factor, effective_scale)]
-            scales.append(effective_scale)
+            scales.append(effective_scale[::-1])
             # get the chunk size for this level
             out_key = 't00000/s00/%i/cells' % (scale + 1,)
             with file_reader(self.output_path, 'r') as f:
-                chunk = f[out_key].chunks
+                chunk = f[out_key].chunks[::-1]
             chunks.append(chunk)
         self._write_metadata_bdv(np.array(scales), np.array(chunks))
         self._write_bdv_xml()
@@ -183,10 +187,9 @@ class WriteDownscalingMetadata(luigi.Task):
 class DownscalingWorkflow(WorkflowBase):
     input_path = luigi.Parameter()
     input_key = luigi.Parameter()
-    output_path = luigi.Parameter()
     scale_factors = luigi.ListParameter()
     halos = luigi.ListParameter()
-    metadata_format = luigi.Parameter(default='custom')
+    metadata_format = luigi.Parameter(default='paintera')
     metadata_dict = luigi.DictParameter(default={})
     output_key_prefix = luigi.Parameter(default='')
 
@@ -205,24 +208,24 @@ class DownscalingWorkflow(WorkflowBase):
         assert all(len(halo) == 3 for halo in halos if halo)
         return halos
 
+    # TODO support more downsampling formats:
+    # imaris
     def validate_format(self):
-        # TODO support more downsampling formats:
-        # n5-viewer, imaris
-        assert self.metadata_format in ('custom', 'bdv')
-        if self.metadata_format == 'custom':
-            assert self.output_key_prefix != '', "Need output_key_prefix for custom data format"
+        assert self.metadata_format in ('paintera', 'bdv'),\
+            "Invalid format %s" % self.metadata_format
+        if self.metadata_format == 'paintera':
+            assert self.output_key_prefix != '',\
+                "Need output_key_prefix for paintera data format"
         # for now, we only support a single 'setup' and a single
         # time-point for the bdv format
         elif self.metadata_format == 'bdv':
-            # TODO we could use hdf5 external links to drop this requirement
-            assert self.input_path == self.output_path
-            assert self.output_key_prefix == '', "Must not give output_key_prefix for custom data format"
-            # TODO do we need to check for some relevant meta-data ???
+            assert self.output_key_prefix == '',\
+                "Must not give output_key_prefix for bdv data format"
 
     # we offset the scale by 1 because
     # 0 indicates the original resoulution
     def get_scale_key(self, scale):
-        if self.metadata_format == 'custom':
+        if self.metadata_format == 'paintera':
             prefix = 's%i' % (scale + 1,)
             out_key = os.path.join(self.output_key_prefix, prefix)
         elif self.metadata_format == 'bdv':
@@ -231,10 +234,16 @@ class DownscalingWorkflow(WorkflowBase):
             out_key = 't00000/s00/%i/cells' % (scale + 1,)
         return out_key
 
-    def _link_scale_zero(self, trgt):
+    def _link_scale_zero_h5(self, trgt):
         with file_reader(self.input_path) as f:
             if trgt not in f:
                 f[trgt] = f[self.input_key]
+
+    def _link_scale_zero_n5(self, trgt):
+        with file_reader(self.input_path) as f:
+            if trgt not in f:
+                os.symlink(os.path.join(self.input_path, self.input_key),
+                           os.path.join(self.input_path, trgt))
 
     def requires(self):
         self.validate_scale_factors(self.scale_factors)
@@ -244,14 +253,11 @@ class DownscalingWorkflow(WorkflowBase):
         ds_task = getattr(downscale_tasks,
                           self._get_task_name('Downscaling'))
 
+        in_key = self.get_scale_key(-1)
         if self.metadata_format == 'bdv':
-            in_path = self.output_path
-            in_key = self.get_scale_key(-1)
-            self._link_scale_zero(in_key)
-        else:
-            t_prev = self.dependency
-            in_path = self.input_path
-            in_key = self.input_key
+            self._link_scale_zero_h5(in_key)
+        elif self.metadata_format == 'paintera':
+            self._link_scale_zero_n5(in_key)
         t_prev = self.dependency
 
         effective_scale = [1, 1, 1]
@@ -266,20 +272,19 @@ class DownscalingWorkflow(WorkflowBase):
             prefix = 's%i' % (scale + 1,)
             t = ds_task(tmp_folder=self.tmp_folder, max_jobs=self.max_jobs,
                         config_dir=self.config_dir,
-                        input_path=in_path, input_key=in_key,
-                        output_path=self.output_path, output_key=out_key,
+                        input_path=self.input_path, input_key=in_key,
+                        output_path=self.input_path, output_key=out_key,
                         scale_factor=scale_factor, scale_prefix=prefix,
                         effective_scale_factor=effective_scale,
                         halo=halos[scale],
                         dependency=t_prev)
 
             t_prev = t
-            in_path = self.output_path
             in_key = out_key
 
         # task to write the metadata
         t_meta = WriteDownscalingMetadata(tmp_folder=self.tmp_folder,
-                                          output_path=self.output_path,
+                                          output_path=self.input_path,
                                           output_key_prefix=self.output_key_prefix,
                                           metadata_format=self.metadata_format,
                                           metadata_dict=self.metadata_dict,
