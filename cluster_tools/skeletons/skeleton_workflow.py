54813faa3aa1699b1d9cd568bbe3e95629bffcc8
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-01-21 16:54:43 +0100

    Update skeleton workflow

--- cluster_tools/skeletons/skeleton_workflow.py
+++ cluster_tools/skeletons/skeleton_workflow.py
@@ -13,25 +13,32 @@ class SkeletonWorkflow(WorkflowBase):
     output_path = luigi.Parameter()
     output_prefix = luigi.Parameter()
     work_scale = luigi.IntParameter()
+    target_scale = luigi.IntParameter(default=None)
 
     def requires(self):
         skel_task = getattr(skeleton_tasks,
                             self._get_task_name('Skeletonize'))
         in_key1 = '%s/s%i' % (self.input_prefix, self.work_scale)
         out_key1 = '%s/s%i' % (self.output_prefix, self.work_scale)
-        t1 = skel_task(tmp_folder=self.tmp_folder,
-                       max_jobs=self.max_jobs,
-                       config_dir=self.config_dir,
-                       dependency=self.dependency,
-                       input_path=self.input_path,
-                       input_key=in_key1,
-                       output_path=self.output_path,
-                       output_key=out_key1)
-        return t1
+        dep = skel_task(tmp_folder=self.tmp_folder,
+                        max_jobs=self.max_jobs,
+                        config_dir=self.config_dir,
+                        dependency=self.dependency,
+                        input_path=self.input_path,
+                        input_key=in_key1,
+                        output_path=self.output_path,
+                        output_key=out_key1)
+
+        # check if we have a target scale that skeletons should get
+        # upsampled to
+        if self.target_scale is None or self.target_scale == self.work_scale:
+            return dep
+        else:
+            assert self.target_scale < self.work_scale
         upsample_task = getattr(upsample_tasks,
                                 self._get_task_name('UpsampleSkeletons'))
-        in_key2 = '%s/s0' % self.input_prefix
-        out_key2 = '%s/s0' % self.output_prefix
+        in_key2 = '%s/s%i' % (self.input_prefix, self.target_scale)
+        out_key2 = '%s/s%i' % (self.output_prefix, self.target_scale)
         t2 = upsample_task(tmp_folder=self.tmp_folder,
                            max_jobs=self.max_jobs,
                            config_dir=self.config_dir,
