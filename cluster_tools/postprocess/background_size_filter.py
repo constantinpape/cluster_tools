4ad284b7b41939277386dab54d7eac50d5e47289
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-01-28 15:50:47 +0100

    Copy maxId in postprocessing workflow

--- cluster_tools/postprocess/background_size_filter.py
+++ cluster_tools/postprocess/background_size_filter.py
@@ -175,6 +175,15 @@ def background_size_filter(job_id, config_path):
             [apply_block(block_id, blocking, ds_in, ds_out, discard_ids)
              for block_id in block_list]
 
+    # copy the 'maxId' attribute if present
+    if job_id == 0:
+        with vu.file_reader(input_path, 'r') as f:
+            attrs = ds[input_key].attrs
+            max_id = attrs.get('maxId', None)
+        if max_id is not None:
+            with vu.file_reader(output_path) as f:
+                f[output_path].attrs['maxId'] = max_id
+
     fu.log_job_success(job_id)
 
 
