6b18da5de0046bd073048cc63e7a434fe2819846
Constantin Pape <c.pape@gmx.net>
2019-04-11 23:00:55 +0200

    Start implementation of nml skeleton parser

--- cluster_tools/utils/skeleton_utils.py
+++ cluster_tools/utils/skeleton_utils.py
@@ -1,5 +1,8 @@
-import os
+from xml.dom import minidom
+import zipfile
 # import csv
+# import os
+
 import numpy as np
 from skan import csr
 
@@ -202,6 +205,92 @@ def write_n5(ds, skel_id, skel_vol, coordinate_offset=None):
     data = np.concatenate(data, axis=0)
     ds.write_chunk((skel_id,), data.astype('uint64'), True)
 
+
+#
+# nml parser
+#
+
+
+def parse_attributes(xml_elem, parse_input):
+    """ Parse xml input:
+
+    Arguments:
+        xml_elem: an XML parsing element containing an "attributes" member
+        parse_input: [["attribute_name", python_type_name],
+                      ["52", int], ["1.234", float], ["neurite", str], ...]
+    Returns:
+        list of python-typed values - [52, 1.234, "neurite", ...]
+    """
+    parse_output = []
+    attributes = xml_elem.attributes
+    for x in parse_input:
+        try:
+            if x[1] == int:
+                # ensure float strings can be parsed too
+                parse_output.append(int(float(attributes[x[0]].value)))
+            else:
+                parse_output.append(x[1](attributes[x[0]].value))
+        except (KeyError, ValueError):
+            parse_output.append(None)
+    return parse_output
+
+
+# Construct annotation. Annotations are trees (called things inside the nml files).
+def read_coords_from_nml(nml):
+    annotation_elems = nml.getElementsByTagName("thing")
+    skeleton_coordinates = {}
+
+    # TODO parse the skeleton id and use as key instead of linear index
+    for skel_id, annotation_elem in enumerate(annotation_elems):
+        node_elems = annotation_elem.getElementsByTagName("node")
+        coords = []
+        for node_elem in node_elems:
+            x, y, z = parse_attributes(node_elem,
+                                       [['x', int], ['y', int], ['z', int]])
+            # TODO is this stored in physical coordinates?
+            # need to transform appropriately
+            coords.append([z, y, x])
+        skeleton_coordinates[skel_id] = coords
+    #
+    return skeleton_coordinates
+
+
+# TODO return additional annotations etc
+# TODO figure out scaling
+def read_nml(input_path):
+    """ Read skeleton stored in nml format
+
+    NML format used by Knossos
+    For details on the nml format see .
+
+    Arguments:
+        input_path [str]: path to swc file
+    """
+    # parse the nml format
+    if input_path.endswith('k.zip'):
+        zipper = zipfile.ZipFile(input_path)
+        if 'annotation.xml' not in zipper.namelist():
+            raise Exception("k.zip file does not contain annotation.xml")
+        xml_string = zipper.read('annotation.xml')
+        nml = minidom.parseString(xml_string)
+    else:
+        nml = minidom.parse(input_path)
+
+    # read the pixel size
+    try:
+        param = nml.getElementsByTagName("parameters")[0].getElementsByTagName("scale")[0]
+        file_scaling = parse_attributes(param,
+                                        [["x", float], ["y", float], ["z", float]])
+    except IndexError:
+        file_scaling = [1, 1, 1]
+
+    skeleton_dict = read_coords_from_nml(nml)
+    # TODO read and return tree structure
+    return skeleton_dict
+
+
+def write_nml():
+    pass
 
 #
 # csv parser
