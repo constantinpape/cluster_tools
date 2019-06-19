9cfe10daea58a34edf407852840cf3894459eb5f
Constantin Pape <c.pape@gmx.net>
2019-06-20 01:56:17 +0200

    Update workflow tests

--- test/workflows/lifted_multicut_workflow.py
+++ test/workflows/lifted_multicut_workflow.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import json
 import unittest
 import numpy as np
@@ -7,41 +6,28 @@ from shutil import rmtree
 
 import luigi
 import z5py
-
-import nifty.tools as nt
-import nifty.graph.rag as nrag
-import nifty.distributed as ndist
-
-try:
-    from cluster_tools import LiftedMulticutSegmentationWorkflow
-except ImportError:
-    sys.path.append('../..')
-    from cluster_tools import LiftedMulticutSegmentationWorkflow
+from cluster_tools import LiftedMulticutSegmentationWorkflow
 
 
 class TestLiftedMulticutWorkflow(unittest.TestCase):
-    input_path = '/home/constantin/Work/data/cluster_tools_test_data/test_data.n5'
-    input_key = 'volumes/boundaries_float32'
-    ws_key = 'volumes/watershed'
+    input_path = '/g/kreshuk/data/cremi/example/sampleA.n5'
+    input_key = 'volumes/affinities'
+    ws_key = 'volumes/segmentation/watershed'
+
     tmp_folder = './tmp'
     config_folder = './tmp/configs'
-    target = 'local'
-    block_shape = [10, 256, 256]
+    out_path = os.path.join(tmp_folder, 'data.n5')
 
-    @staticmethod
-    def _mkdir(dir_):
-        try:
-            os.mkdir(dir_)
-        except OSError:
-            pass
+    target = 'local'
+    block_shape = [25, 256, 256]
+    shebang = '#! /g/kreshuk/pape/Work/software/conda/miniconda3/envs/cluster_env37/bin/python'
 
     def setUp(self):
-        self._mkdir(self.tmp_folder)
-        self._mkdir(self.config_folder)
+        os.makedirs(self.tmp_folder, exist_ok=True)
+        os.makedirs(self.config_folder, exist_ok=True)
         config = LiftedMulticutSegmentationWorkflow.get_config()
         global_config = config['global']
-        # global_config['shebang'] = '#! /g/kreshuk/pape/Work/software/conda/miniconda3/envs/cluster_env/bin/python'
-        global_config['shebang'] = '#! /home/constantin/Work/software/conda/miniconda3/envs/main/bin/python'
+        global_config['shebang'] = self.shebang
         global_config['block_shape'] = self.block_shape
         with open(os.path.join(self.config_folder, 'global.config'), 'w') as f:
             json.dump(global_config, f)
@@ -53,37 +39,31 @@ class TestLiftedMulticutWorkflow(unittest.TestCase):
             pass
 
     def _check_result(self):
-        mc_path = self.tmp_folder + '/lmc.n5'
-        with z5py.File(mc_path) as f:
+        with z5py.File(self.out_path) as f:
             node_labels = f['node_labels'][:]
             mc = f['volumes/lifted_multicut'][:]
-        self.assertEqual(mc.shape, (30, 512, 512))
+        exp_shape = z5py.File(self.input_path)[self.ws_key].shape
+
+        self.assertEqual(mc.shape, exp_shape)
         unique_nodes = np.unique(node_labels)
         unique_segments = np.unique(mc)
         self.assertTrue(np.allclose(unique_nodes, unique_segments))
         self.assertGreater(len(unique_nodes), 20)
 
     def test_workflow(self):
+        task = LiftedMulticutSegmentationWorkflow
         max_jobs = 8
-        mc_path = self.tmp_folder + '/lmc.n5'
-        ret = luigi.build([LiftedMulticutSegmentationWorkflow(input_path=self.input_path,
-                                                              input_key=self.input_key,
-                                                              ws_path=self.input_path,
-                                                              ws_key=self.ws_key,
-                                                              problem_path=mc_path,
-                                                              node_labels_key='node_labels',
-                                                              output_path=mc_path,
-                                                              output_key='volumes/lifted_multicut',
-                                                              lifted_labels_path=self.input_path,
-                                                              lifted_labels_key='volumes/test_labels',
-                                                              lifted_prefix='test',
-                                                              n_scales=1,
-                                                              skip_ws=True,
-                                                              config_dir=self.config_folder,
-                                                              tmp_folder=self.tmp_folder,
-                                                              target=self.target,
-                                                              max_jobs=max_jobs)],
-                          local_scheduler=True)
+        t = task(input_path=self.input_path, input_key=self.input_key,
+                 ws_path=self.input_path, ws_key=self.ws_key,
+                 problem_path=self.out_path, node_labels_key='node_labels',
+                 output_path=self.out_path, output_key='volumes/lifted_multicut',
+                 lifted_labels_path=self.input_path,
+                 lifted_labels_key='volumes/segmentation/groundtruth',
+                 lifted_prefix='test', n_scales=1, skip_ws=True,
+                 config_dir=self.config_folder, tmp_folder=self.tmp_folder,
+                 target=self.target, max_jobs=max_jobs)
+
+        ret = luigi.build([t], local_scheduler=True)
         self.assertTrue(ret)
         self._check_result()
 
--- test/workflows/multicut_workflow.py
+++ test/workflows/multicut_workflow.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import json
 import unittest
 import numpy as np
@@ -7,41 +6,29 @@ from shutil import rmtree
 
 import luigi
 import z5py
-
-import nifty.tools as nt
-import nifty.graph.rag as nrag
-import nifty.distributed as ndist
-
-try:
-    from cluster_tools import MulticutSegmentationWorkflow
-except ImportError:
-    sys.path.append('../..')
-    from cluster_tools import MulticutSegmentationWorkflow
+from cluster_tools import MulticutSegmentationWorkflow
 
 
 class TestMulticutWorkflow(unittest.TestCase):
-    input_path = '/home/constantin/Work/data/cluster_tools_test_data/test_data.n5'
-    input_key = 'volumes/boundaries_float32'
-    ws_key = 'volumes/watershed'
+    input_path = '/g/kreshuk/data/cremi/example/sampleA.n5'
+    input_key = 'volumes/affinities'
+    ws_key = 'volumes/segmentation/watershed'
+
     tmp_folder = './tmp'
+    out_path = os.path.join(tmp_folder, 'data.n5')
+
     config_folder = './tmp/configs'
     target = 'local'
-    block_shape = [10, 256, 256]
-
-    @staticmethod
-    def _mkdir(dir_):
-        try:
-            os.mkdir(dir_)
-        except OSError:
-            pass
+    block_shape = [25, 256, 256]
+    shebang = '#! /g/kreshuk/pape/Work/software/conda/miniconda3/envs/cluster_env37/bin/python'
 
     def setUp(self):
-        self._mkdir(self.tmp_folder)
-        self._mkdir(self.config_folder)
+        os.makedirs(self.tmp_folder, exist_ok=True)
+        os.makedirs(self.config_folder, exist_ok=True)
         config = MulticutSegmentationWorkflow.get_config()
+
         global_config = config['global']
-        # global_config['shebang'] = '#! /g/kreshuk/pape/Work/software/conda/miniconda3/envs/cluster_env/bin/python'
-        global_config['shebang'] = '#! /home/constantin/Work/software/conda/miniconda3/envs/main/bin/python'
+        global_config['shebang'] = self.shebang
         global_config['block_shape'] = self.block_shape
         with open(os.path.join(self.config_folder, 'global.config'), 'w') as f:
             json.dump(global_config, f)
@@ -53,11 +40,11 @@ class TestMulticutWorkflow(unittest.TestCase):
             pass
 
     def _check_result(self):
-        mc_path = self.tmp_folder + '/mc.n5'
-        with z5py.File(mc_path) as f:
+        exp_shape = z5py.File(self.path)[self.ws_key].shape
+        with z5py.File(self.out_path) as f:
             node_labels = f['node_labels'][:]
             mc = f['volumes/multicut'][:]
-        self.assertEqual(mc.shape, (30, 512, 512))
+        self.assertEqual(mc.shape, exp_shape)
         unique_nodes = np.unique(node_labels)
         unique_segments = np.unique(mc)
         self.assertTrue(np.allclose(unique_nodes, unique_segments))
@@ -65,22 +52,15 @@ class TestMulticutWorkflow(unittest.TestCase):
 
     def test_workflow(self):
         max_jobs = 8
-        mc_path = self.tmp_folder + '/mc.n5'
-        ret = luigi.build([MulticutSegmentationWorkflow(input_path=self.input_path,
-                                                        input_key=self.input_key,
-                                                        ws_path=self.input_path,
-                                                        ws_key=self.ws_key,
-                                                        problem_path=mc_path,
-                                                        node_labels_key='node_labels',
-                                                        output_path=mc_path,
-                                                        output_key='volumes/multicut',
-                                                        n_scales=1,
-                                                        skip_ws=True,
-                                                        config_dir=self.config_folder,
-                                                        tmp_folder=self.tmp_folder,
-                                                        target=self.target,
-                                                        max_jobs=max_jobs)],
-                          local_scheduler=True)
+        task = MulticutSegmentationWorkflow
+        t = task(input_path=self.input_path, input_key=self.input_key,
+                 ws_path=self.input_path, ws_key=self.ws_key,
+                 problem_path=self.out_path, node_labels_key='node_labels',
+                 output_path=self.out_path, output_key='volumes/multicut',
+                 n_scales=1, skip_ws=True,
+                 config_dir=self.config_folder, tmp_folder=self.tmp_folder,
+                 target=self.target, max_jobs=max_jobs)
+        ret = luigi.build([t], local_scheduler=True)
         self.assertTrue(ret)
         self._check_result()
 
