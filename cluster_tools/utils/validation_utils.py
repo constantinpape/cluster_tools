0eaebda92e64ec73be24a3258d1ee23de3c8cb92
Constantin Pape <c.pape@gmx.net>
2019-06-19 13:46:44 +0200

    Start to refactor validation WIP

--- cluster_tools/utils/validation_utils.py
+++ cluster_tools/utils/validation_utils.py
@@ -1,63 +1,119 @@
-import json
+import numpy as np
 
 
-class SegmentationValidation:
+def contigency_table(seg_a, seg_b):
+    """ Compute the pairs and counts in the contingency table of seg_a and seg_b.
 
-    def load_rand_primitives(self, eval_dict):
-        assert len({'randA', 'randB', 'randAB'} - set(eval_dict.keys())) == 0, "Not all rand primitives present"
-        self.rand_a = eval_dict['randA']
-        self.rand_b = eval_dict['randB']
-        self.rand_ab = eval_dict['randAB']
+    The contingency table counts the number of pixels that are shared between
+    objects from seg_a and seg_b
+    """
+    # compute the unique ids and couunts for seg a and seg b
+    # and wrap them in a dict
+    a_ids, a_counts = np.unique(seg_a, return_counts=True)
+    b_ids, b_counts = np.unique(seg_b, return_counts=True)
+    a_dict = dict(zip(a_ids, a_counts))
+    b_dict = dict(zip(b_ids, b_counts))
+    # compute the overlaps and overlap counts
+    pairs = np.rec.fromrecords(zip(a_ids, b_ids))
+    p_ids, p_counts = np.unique(pairs, return_counts=True)
+    return a_dict, b_dict, p_ids, p_counts
 
-    def load_vi_primitives(self, eval_dict):
-        assert len({'viA', 'viB', 'viAB'} - set(eval_dict.keys())) == 0, "Not all vi primitives present"
-        self.vi_a = eval_dict['viA']
-        self.vi_b = eval_dict['viB']
-        self.vi_ab = eval_dict['viAB']
 
-    def __init__(self, path):
-        with open(path, 'r') as f:
-            eval_dict = json.load(f)
-        self.load_rand_primitives(eval_dict)
-        self.load_vi_primitives(eval_dict)
-        assert 'nPoints' in eval_dict, "Number of points not present"
-        self.n_points = eval_dict['nPoints']
+def compute_ignore_mask(seg_a, seg_b, ignore_a, ignore_b):
+    if ignore_a is None and ignore_b is None:
+        return None
+    ignore_mask_a = None if ignore_a is None else np.isin(seg_a, ignore_a)
+    ignore_mask_b = None if ignore_b is None else np.isin(seg_b, ignore_b)
 
-    #
-    # rand measures
-    #
+    if ignore_mask_a is not None and ignore_mask_b is None:
+        ignore_mask = ignore_mask_a
+    elif ignore_mask_a is None and ignore_mask_b is not None:
+        ignore_mask = ignore_mask_b
+    elif ignore_mask_a is not None and ignore_mask_b is not None:
+        ignore_mask = np.logical_and(ignore_mask_a, ignore_mask_b)
 
-    @property
-    def rand_index(self):
-        return 1. - (self.rand_a + self.rand_b - 2 * self.rand_ab) / (self.n_points * self.n_points)
+    # need to invert the mask
+    return np.logical_not(ignore_mask)
 
-    @property
-    def rand_precision(self):
-        return self.rand_ab / self.rand_b
 
-    @property
-    def rand_recall(self):
-        return self.rand_ab / self.rand_a
+def compute_vi_scores(a_dict, b_dict, p_ids, p_counts, n_points, use_log2):
 
-    @property
-    def adapated_rand_score(self):
-        prec = self.rand_precision
-        rec = self.rand_recall
-        return 2. * prec * rec / (prec + rec)
+    log = np.log2 if use_log2 else np.log
 
-    #
-    # vi measures
-    #
+    # compute the vi-primitves
+    a_counts = a_dict.values()
+    sum_a = sum(-c / n_points * log(c / n_points) for c in a_counts)
 
-    @property
-    def variation_of_information(self):
-        return self.vi_a + self.vi_b - 2. * self.vi_a
+    b_counts = b_dict.values()
+    sum_b = sum(-c / n_points * log(c / n_points) for c in b_counts)
 
-    # TODO
-    @property
-    def vi_merge(self):
-        return
+    sum_ab = np.sum([c / n_points * log(n_points * c / (a_dict[a] * b_dict[b]))
+                     for (a, b), c in zip(p_ids, p_counts)])
+    # compute the actual vi-scores (split-vi, merge-vi)
+    vis = sum_a - sum_ab
+    vim = sum_b - sum_ab
+    return vis, vim
 
-    @property
-    def vi_split(self):
-        return
+
+def variation_of_information(seg_a, seg_b, ignore_a=None, ignore_b=None, use_log2=True):
+    """ Compute variation of information between two segmentations.
+
+    Computes split and merge vi, add them up to get the full vi score.
+
+    Arguments:
+        seg_a [np.ndarray] - segmentation a
+        seg_b [np.ndarray] - segmentation b
+        ignore_a [listlike] - ignore ids for segmentation a (default: None)
+        ignore_b [listlike] - ignore ids for segmentation b (default: None)
+        use_log2 [bool] - whether to use log2 or loge (default: True)
+    Retuns:
+        float - split vi
+        float - merge vi
+    """
+    ignore_mask = compute_ignore_mask(seg_a, seg_b, ignore_a, ignore_b)
+    if ignore_mask is not None:
+        seg_a = seg_a[ignore_mask]
+        seg_b = seg_b[ignore_mask]
+
+    # compute ids, counts and overlaps making up the contigency table
+    a_dict, b_dict, p_ids, p_counts = contigency_table(seg_a, seg_b)
+    n_points = seg_a.size
+
+    # compute and return vi scores
+    vis, vim = compute_vi_scores(a_dict, b_dict, p_ids, p_counts, n_points,
+                                 use_log2=use_log2)
+    return vis, vim
+
+
+# TODO
+def compute_rand_scores():
+    raise NotImplementedError("Rand not implemented yet")
+
+
+# TODO
+def rand_index(seg_a, seg_b, ignore_a=None, ignore_b=None):
+    """ Compute rand index derived scores between to segmentations.
+
+    Computes split and merge vi, add them up to get the full vi score
+
+    Arguments:
+        seg_a [np.ndarray] - segmentation a
+        seg_b [np.ndarray] - segmentation b
+        ignore_a [listlike] - ignore ids for segmentation a (default: None)
+        ignore_b [listlike] - ignore ids for segmentation b (default: None)
+    Retuns:
+        TODO
+    """
+    raise NotImplementedError("Rand not implemented yet")
+    ignore_mask = compute_ignore_mask(seg_a, seg_b, ignore_a, ignore_b)
+    if ignore_mask is not None:
+        seg_a = seg_a[ignore_mask]
+        seg_b = seg_b[ignore_mask]
+
+    # compute ids, counts and overlaps making up the contigency table
+    a_dict, b_dict, p_ids, p_counts = contigency_table(seg_a, seg_b)
+    # n_points = seg_a.size
+
+    # compute and return vi scores
+    compute_rand_scores()
+    return
