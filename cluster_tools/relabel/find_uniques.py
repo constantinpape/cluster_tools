aac69381b16897d44b0fa8c31b398787f4ead691
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-06-28 14:07:55 +0200

    Update unique task

--- cluster_tools/relabel/find_uniques.py
+++ cluster_tools/relabel/find_uniques.py
@@ -4,8 +4,13 @@ import os
 import sys
 import json
 
-import luigi
+# this is a task called by multiple processes,
+# so we need to restrict the number of threads used by numpy
+from cluster_tools.utils.numpy_utils import set_numpy_threads
+set_numpy_threads(1)
 import numpy as np
+
+import luigi
 import nifty.tools as nt
 
 import cluster_tools.utils.volume_utils as vu
@@ -90,6 +95,11 @@ def uniques_in_block(block_id, blocking, ds, return_counts):
     block = blocking.getBlock(block_id)
     bb = vu.block_to_bb(block)
     labels = ds[bb]
+
+    if labels.sum() == 0:
+        if return_counts:
+            return np.array([0]), np.array([labels.size])
+        return np.array([0])
 
     if return_counts:
         uniques, counts = np.unique(labels, return_counts=True)
