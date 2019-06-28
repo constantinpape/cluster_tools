3009ba461f983ee32c512704b99bb04aac26e499
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-06-28 14:07:16 +0200

    Add utils to restrict number of threads used by numpy

---
+++ cluster_tools/utils/numpy_utils.py
@@ -0,0 +1,31 @@
+import os
+import ctypes
+
+
+def set_numpy_threads(n_threads):
+    """ Set the number of threads numpy exposes to its
+    underlying linalg library.
+
+    Based on https://github.com/numpy/numpy/issues/11826.
+    """
+
+    # set number of threads for mkl if it is used
+    try:
+        import mkl
+        mkl.set_num_threaads(n_threads)
+    except Exception:
+        pass
+
+    for name in ['libmkl_rt.so', 'libmkl_rt.dylib', 'mkl_Rt.dll']:
+        try:
+            mkl_rt = ctypes.CDLL(name)
+            mkl_rt.mkl_set_num_threads(ctypes.byref(ctypes.c_int(n_threads)))
+        except Exception:
+            pass
+
+    # set number of threads in all possibly relevant environment variables
+    os.environ['OMP_NUM_THREADS'] = str(n_threads)
+    os.environ['OPENBLAS_NUM_THREADS'] = str(n_threads)
+    os.environ['MKL_NUM_THREADS'] = str(n_threads)
+    os.environ['VECLIB_NUM_THREADS'] = str(n_threads)
+    os.environ['NUMEXPR_NUM_THREADS'] = str(n_threads)
