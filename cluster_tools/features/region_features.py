44ee2bfc4b123c4f21134d952f543cd33f87bd5d
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-06-29 11:12:13 +0200

    Set numpy threads to 1 in region features

--- cluster_tools/features/region_features.py
+++ cluster_tools/features/region_features.py
@@ -4,7 +4,12 @@ import os
 import sys
 import json
 
+# this is a task called by multiple processes,
+# so we need to restrict the number of threads used by numpy
+from cluster_tools.utils.numpy_utils import set_numpy_threads
+set_numpy_threads(1)
 import numpy as np
+
 import luigi
 import z5py
 import nifty.tools as nt
