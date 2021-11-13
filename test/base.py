0d29fa3eb2bae78f88420861573aa5ae4f61967f
Constantin Pape <c.pape@gmx.net>
2021-11-13 10:52:06 +0100

    Add prepare_test function and add github CI

--- test/base.py
+++ test/base.py
@@ -3,40 +3,68 @@ import multiprocessing
 import os
 import unittest
 from shutil import rmtree
+from zipfile import ZipFile
 
 import luigi
 from cluster_tools.cluster_tasks import BaseClusterTask
 from cluster_tools.graph import GraphWorkflow
 
-INPUT_PATH = os.environ.get('CLUSTER_TOOLS_TEST_PATH',
-                            '/g/kreshuk/data/cremi/example/sampleA.n5')
-SHEBANG = os.environ.get('CLUSTER_TOOLS_TEST_SHEBANG',
-                         '#! /g/kreshuk/pape/Work/software/conda/miniconda3/envs/cluster_env37/bin/python')
-MAX_JOBS = os.environ.get('CLUSTER_TOOS_TEST_MAX_JOBS', min(multiprocessing.cpu_count(), 16))
-TARGET = os.environ.get('CLUSTER_TOOLS_TEST_TARGET', 'local')
+INPUT_PATH = os.environ.get("CLUSTER_TOOLS_TEST_PATH",
+                            os.path.join(os.path.split(__file__)[0], "../test_data/sampleA.n5"))
+SHEBANG = os.environ.get("CLUSTER_TOOLS_TEST_SHEBANG", None)
+MAX_JOBS = os.environ.get("CLUSTER_TOOS_TEST_MAX_JOBS", min(multiprocessing.cpu_count(), 16))
+TARGET = os.environ.get("CLUSTER_TOOLS_TEST_TARGET", "local")
+
+
+def prepare_test():
+    import z5py
+
+    if os.path.exists(INPUT_PATH):
+        print("Test data at", INPUT_PATH, "exists already")
+    else:
+        import gdown
+
+        # download the test data
+        print("Downloading test data to", INPUT_PATH)
+        out_folder = os.path.split(INPUT_PATH)[0]
+        out_path = os.path.join(out_folder, "tmp.zip")
+        os.makedirs(out_folder, exist_ok=True)
+        url = "https://drive.google.com/u/0/uc?export=download&confirm=crb1&id=1E_Wpw9u8E4foYKk7wvx5RPSWvg_NCN7U"
+        gdown.download(url, out_path, quiet=False)
+
+        # unzip it
+        with ZipFile(out_path, "r") as f:
+            f.extractall(out_folder)
+        assert os.path.exists(INPUT_PATH)
+
+        # clean up
+        os.remove(out_path)
+
+    with z5py.File(INPUT_PATH, "r") as f:
+        assert "volumes/raw" in f, "Test data is invalid"
 
 
 class BaseTest(unittest.TestCase):
-    input_path = INPUT_PATH
+    input_path = os.path.abspath(INPUT_PATH)
     shebang = SHEBANG
     max_jobs = MAX_JOBS
     target = TARGET
 
-    tmp_folder = './tmp'
-    config_folder = './tmp/config'
-    output_path = './tmp/data.n5'
+    tmp_folder = "./tmp"
+    config_folder = "./tmp/config"
+    output_path = "./tmp/data.n5"
     block_shape = [32, 256, 256]
 
-    graph_key = 'graph'
-    ws_key = 'volumes/segmentation/watershed'
-    boundary_key = 'volumes/boundaries'
-    aff_key = 'volumes/affinities'
+    graph_key = "graph"
+    ws_key = "volumes/segmentation/watershed"
+    boundary_key = "volumes/boundaries"
+    aff_key = "volumes/affinities"
 
     def setUp(self):
         os.makedirs(self.config_folder, exist_ok=True)
         config = BaseClusterTask.default_global_config()
-        config.update({'shebang': self.shebang, 'block_shape': self.block_shape})
-        with open(os.path.join(self.config_folder, 'global.config'), 'w') as f:
+        config.update({"shebang": self.shebang, "block_shape": self.block_shape})
+        with open(os.path.join(self.config_folder, "global.config"), "w") as f:
             json.dump(config, f)
 
     def tearDown(self):
@@ -48,9 +76,9 @@ class BaseTest(unittest.TestCase):
     def compute_graph(self, ignore_label=True):
         task = GraphWorkflow
 
-        config = task.get_config()['initial_sub_graphs']
-        config.update({'ignore_label': ignore_label})
-        with open(os.path.join(self.config_folder, 'initial_sub_graphs.config'), 'w') as f:
+        config = task.get_config()["initial_sub_graphs"]
+        config.update({"ignore_label": ignore_label})
+        with open(os.path.join(self.config_folder, "initial_sub_graphs.config"), "w") as f:
             json.dump(config, f)
 
         ret = luigi.build([task(input_path=self.input_path,
@@ -65,5 +93,9 @@ class BaseTest(unittest.TestCase):
         self.assertTrue(ret)
 
     def get_target_name(self):
-        name_dict = {'local': 'Local', 'slurm': 'Slurm', 'lsf': 'LSF'}
+        name_dict = {"local": "Local", "slurm": "Slurm", "lsf": "LSF"}
         return name_dict[self.target]
+
+
+if __name__ == "__main__":
+    prepare_test()
